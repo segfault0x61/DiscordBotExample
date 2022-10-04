b4ebe2949af3e8d204f45dcfc3a021eb8779e934
segfault0x61 <torresj1997@outlook.com>
2022-10-04 16:20:40 -0500

    feat: another command and descriptions

--- DiscordBotExample/Commands/FunCommands.cs
+++ DiscordBotExample/Commands/FunCommands.cs
@@ -6,9 +6,19 @@ namespace DiscordBotExample.Commands
     public class FunCommands : BaseCommandModule
     {
         [Command("ping")]
+        [Description("Returns pong.")]
         public async Task Ping(CommandContext ctx)
         {
             await ctx.RespondAsync("Pong!").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Adds two numbers together.")]
+        public async Task Add(CommandContext ctx, [Description("First Number")] int num1,
+            [Description("Second Number")] int num2)
+
+        {
+            await ctx.RespondAsync((num1 + num2).ToString()).ConfigureAwait(false);
         }
     }
 }
