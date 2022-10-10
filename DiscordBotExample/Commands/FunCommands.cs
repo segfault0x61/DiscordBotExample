03cf7e238bcf91165d3122fb6b51c6b732f9655a
segfault0x61 <torresj1997@outlook.com>
2022-10-10 10:07:15 -0500

    feat: add poll command

--- DiscordBotExample/Commands/FunCommands.cs
+++ DiscordBotExample/Commands/FunCommands.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.EventHandling;
 using DSharpPlus.Interactivity.Extensions;
 
 namespace DiscordBotExample.Commands
@@ -40,6 +42,33 @@ namespace DiscordBotExample.Commands
             var message = await interactivity.WaitForReactionAsync(x => x.Channel == ctx.Channel && x.User == ctx.User).ConfigureAwait(false);
 
             await ctx.Channel.SendMessageAsync(message.Result.Emoji);
+        }
+
+        [Command("poll")]
+        public async Task Poll(CommandContext ctx, TimeSpan duration, params DiscordEmoji[] emojiOptions)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var options = emojiOptions.Select(x => x.ToString());
+
+            var pollEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Poll",
+                Description = string.Join(" ", options),
+                Color = DiscordColor.Azure
+            };
+
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
+
+            foreach (var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+
+            var results = result.OrderByDescending(x => x.Total).Distinct().Select(x => $"{x.Emoji}: {x.Total}");
+
+            await ctx.Channel.SendMessageAsync(string.Join("\n", results)).ConfigureAwait(false);
         }
     }
 }
